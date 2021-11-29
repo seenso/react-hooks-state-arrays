d7b977a8b500fd75cc02266b07ed60846a9aeb31
seenso <>
2021-11-29 07:38:30 -0900

    Completed assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,18 +3,56 @@ import { spicyFoods, getNewSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState("All");
+
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value);
+  }
+
+  const foodsToDisplay = foods.filter(food => {
+    if (filterBy === "All") {
+      return true;
+    } else {
+      return food.cuisine === filterBy;
+    }
+  });
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
     console.log(newFood);
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray); 
+    //^ setting new list to be equal to copy of original + newFood
   }
+
+  function handleLiClick(id) {
+    const newFoodArray = foods.filter((food) => food.id !== id);
+    //Calling .filter will return a new array based on which elements match our criteria in the callback function.
+    //in this case, it looks for foods that do NOT match the food.id we are trying to remove.
+    setFoods(newFoodArray);
+  }
+
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
+      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+    </li>
+  ))
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{/* list of spicy foods */}</ul>
+      <select name="filter" onChange={handleFilterChange}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
+      <ul>{foodList}</ul>
     </div>
   );
+
+
 }
 
 export default SpicyFoodList;
